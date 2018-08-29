cadca3ee68c2ba991fdf2ee9f0bb1f4345a5ea75
Kazuyuki Iijima <iijimakazuyuki@gmail.com>
2018-08-29 21:02:06 +0900

    Add a feature of handling GitLab API paging

--- progress/commits_counter.js
+++ progress/commits_counter.js
@@ -1,20 +1,22 @@
 'use strict';
 
-const https = require('https')
+const https = require('https');
+const { URL } = require('url');
+const PER_PAGE = 100;
 
 const commitsApi = id => `/api/v4/projects/${id}/repository/commits`;
-const commitsApiQuery = (since, until) => `?since=${since}&until=${until}&with_stats=true`;
+const commitsApiQuery = (since, until) => `?since=${since}&until=${until}&with_stats=true&per_page=${PER_PAGE}`;
 
 class CommitsCounter {
     constructor(gitlabHost, token) {
         this.gitlabHost = gitlabHost;
         this.token = token;
     }
-    getCommits(id, since, until) {
+    getPagedCommits(path) {
         return new Promise((resolve, reject) => {
             https.get({
                 hostname: this.gitlabHost,
-                path: commitsApi(id) + commitsApiQuery(since, until),
+                path: path,
                 headers: {
                     'PRIVATE-TOKEN': this.token,
                 },
@@ -25,15 +27,29 @@ class CommitsCounter {
                     if (res.statusCode != 200) {
                         reject(rawData);
                     }
-                    resolve(rawData);
+                    let data = JSON.parse(rawData);
+                    if (res.headers.link) {
+                        let nextMatchResult = /<([^>]+?)>; rel="next"/.exec(res.headers.link);
+                        if (nextMatchResult) {
+                            let nextUrl = new URL(nextMatchResult[1]);
+                            this.getPagedCommits(`${nextUrl.pathname}${nextUrl.search}`).then(nextData =>
+                                resolve(data.concat(nextData))
+                            );
+                            return;
+                        }
+                    }
+                    resolve(data);
                 });
-            }).on('error', e => reject(e));
+            });
         });
+    }
+    getCommits(id, since, until) {
+        return this.getPagedCommits(`${commitsApi(id)}${commitsApiQuery(since, until)}`);
     }
     getStats(id, since, until) {
         return new Promise((resolve, reject) => {
             this.getCommits(id, since, until).then(r =>
-                resolve(JSON.parse(r).reduce((ret, cur) => {
+                resolve(r.reduce((ret, cur) => {
                     let authorName = cur['author_name'];
                     let stats = cur['stats'];
                     if (ret[authorName]) {
